70c7876b12623b1217dbeba2ede05ea9c73c6a1e
Rupam Mondal <rupam8906@gmail.com>
2024-07-31 11:22:31 +0530

    added new features

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ input.addEventListener("keypress", (e)=> {
 
 async function check() {
     const apikey = "03685ded366bb9a73404f7b061af8c0d";
+    const main = document.querySelector(".main")
     const location = document.querySelector("#search").value
     const weatherimg = document.querySelector(".weatherimg")
     const temp = document.querySelector(".temp")
@@ -23,6 +24,8 @@ async function check() {
     const found = document.querySelector(".founded")
     const humidity = document.querySelector(".humidity")
     const wind = document.querySelector(".wind")
+    const video = document.querySelector("video")
+    // console.log(video)
 
 
     if (found.classList.contains("active")) {
@@ -41,17 +44,17 @@ async function check() {
         wind.classList.remove("act")
         wind.classList.add("samech")
     }
-    if (found.id.includes("block")) {
-        found.removeAttribute("id");
-    }
-    if (location_not_found.id.includes("block")) {
-        location_not_found.removeAttribute("id");
-    }
+   
 
     if(location == ""){
+        if (found.id.includes("block")) {
+            found.removeAttribute("id");
+        }
+        main.style.height = "16rem"
         document.querySelector(".locname").innerHTML = "Please enter a location"
         location_not_found.setAttribute("id","block")
         document.body.style.backgroundImage = "url('assets/default.png')"
+        video.src = ""
         setTimeout(() => {
             location_not_found.classList.add("activet")
             location_not_found.classList.remove("location-not-found")
@@ -61,19 +64,25 @@ async function check() {
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apikey}`
     let data = await fetch(`${url}`).then(response => response.json())
     if(data.cod === "404" || data.cod === "400"){
+        if (found.id.includes("block")) {
+            found.removeAttribute("id");
+        }
+        main.style.height = "16rem"
         document.querySelector(".locname").innerHTML = "Sorry, Location not found!!!"
         location_not_found.setAttribute("id","block")
         document.body.style.backgroundImage = "url('assets/default.png')"
+        video.src = ""
         setTimeout(() => {
             location_not_found.classList.add("activet")
             location_not_found.classList.remove("location-not-found")
         }, 1000);
         return;
     }
-    // if (location_not_found.classList.contains("activet")) {
-    //     location_not_found.classList.remove("activet")
-    //     location_not_found.classList.add("location-not-found")
-    // }
+  
+    if (location_not_found.id.includes("block")) {
+        location_not_found.removeAttribute("id");
+    }
+    main.style.height = "23rem"
     found.setAttribute("id","block")
     setTimeout(() => {
         found.classList.add("active")
@@ -86,27 +95,33 @@ async function check() {
     switch (data.weather[0].main) {
         case "Clouds":
             weatherimg.src = "assets/cloud.png"
-            document.body.style.backgroundImage = "url('assets/cloudweather.jpg')"
+            document.body.style.backgroundImage = "url()"
+            video.src = "assets/cloud.mp4"
             break;
         case "Clear":
             weatherimg.src = "assets/clear.png"
-            document.body.style.backgroundImage = "url('assets/clearcloud.jpg')"
+            document.body.style.backgroundImage = "url()"
+            video.src = "assets/clear.mp4"
             break;
         case "Mist":
             weatherimg.src = "assets/mist.png"
-            document.body.style.backgroundImage = "url('assets/mistcloud.png')"
+            document.body.style.backgroundImage = "url()"
+            video.src = "assets/mist.mp4"
             break;
         case "Rain":
             weatherimg.src = "assets/rain.png"
-            document.body.style.backgroundImage = "url('assets/rainweather.jpg')"
+            document.body.style.backgroundImage = "url()"
+            video.src = "assets/rain.mp4"
             break;
         case "Snow":
             weatherimg.src = "assets/snow.png"
-            document.body.style.backgroundImage = "url('assets/snowweather.png')"
+            document.body.style.backgroundImage = "url()"
+            video.src = "assets/snow.mp4"
             break;
         case "Haze" :
             weatherimg.src = "assets/haze.png"
-            document.body.style.backgroundImage = "url('assets/hazeweather.jpg')"
+            document.body.style.backgroundImage = "url()"
+            video.src = "assets/haze.mp4"
             break;
     }
     setTimeout(() => {
@@ -115,6 +130,5 @@ async function check() {
         wind.classList.add("act")
         wind.classList.add("samech")
     }, 1500);
-    // console.log(data)
 }
 
